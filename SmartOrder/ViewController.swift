6caa54a78087803799e6518899dbe665baeaacbd
Jill007798 <104267284+Jill007798@users.noreply.github.com>
2022-08-18 11:43:40 +0800

    Add icon assets

--- SmartOrder/ViewController.swift
+++ SmartOrder/ViewController.swift
@@ -10,9 +10,17 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SOMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //TODO:: user's location
+        let coordinate = CLLocationCoordinate2D(latitude: 25.08, longitude: 121.52)
+        let region = self.SOMapView.regionThatFits(MKCoordinateRegion(center: coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000))
+        self.SOMapView.setRegion(region, animated: true)
+        
     }
 
 
